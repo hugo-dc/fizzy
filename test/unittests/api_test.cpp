33ccbbb9e808df2dd537ae068372a6bc6d5d0dca
Andrei Maiboroda <andrei@ethereum.org>
2020-02-18 15:32:01 +0100

    Implement API to find exported global by name

--- test/unittests/api_test.cpp
+++ test/unittests/api_test.cpp
@@ -1,5 +1,7 @@
 #include "execute.hpp"
+#include "parser.hpp"
 #include <gtest/gtest.h>
+#include <test/utils/hex.hpp>
 
 using namespace fizzy;
 
@@ -34,4 +36,49 @@ TEST(api, find_exported_function)
     EXPECT_FALSE(find_exported_function(module, "mem"));
     EXPECT_FALSE(find_exported_function(module, "glob"));
     EXPECT_FALSE(find_exported_function(module, "table"));
+}
+
+TEST(api, find_exported_global)
+{
+    /* wat2wasm
+    (module
+      (func $f (export "f") nop)
+      (global (export "g1") (mut i32) (i32.const 0))
+      (global (export "g2") i32 (i32.const 1))
+      (global (export "g3") (mut i32) (i32.const 2))
+      (global (export "g4") i32 (i32.const 3))
+      (table (export "tab") 0 anyfunc)
+      (memory (export "mem") 0)
+    )
+     */
+    const auto wasm = from_hex(
+        "0061736d010000000104016000000302010004040170000005030100000615047f0141000b7f0041010b7f0141"
+        "020b7f0041030b072507016600000267310300026732030102673303020267340303037461620100036d656d02"
+        "000a05010300010b");
+
+    auto instance = instantiate(parse(wasm));
+
+    auto opt_global = find_exported_global(instance, "g1");
+    ASSERT_TRUE(opt_global);
+    EXPECT_EQ(*opt_global->value, 0);
+    EXPECT_TRUE(opt_global->is_mutable);
+
+    opt_global = find_exported_global(instance, "g2");
+    ASSERT_TRUE(opt_global);
+    EXPECT_EQ(*opt_global->value, 1);
+    EXPECT_FALSE(opt_global->is_mutable);
+
+    opt_global = find_exported_global(instance, "g3");
+    ASSERT_TRUE(opt_global);
+    EXPECT_EQ(*opt_global->value, 2);
+    EXPECT_TRUE(opt_global->is_mutable);
+
+    opt_global = find_exported_global(instance, "g4");
+    ASSERT_TRUE(opt_global);
+    EXPECT_EQ(*opt_global->value, 3);
+    EXPECT_FALSE(opt_global->is_mutable);
+
+    EXPECT_FALSE(find_exported_global(instance, "f"));
+    EXPECT_FALSE(find_exported_global(instance, "tab"));
+    EXPECT_FALSE(find_exported_global(instance, "mem"));
 }
