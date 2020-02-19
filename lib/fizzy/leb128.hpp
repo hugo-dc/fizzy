695a822abd3a617fe5b6ff5f5313773b8cb7f1ba
Andrei Maiboroda <andrei@ethereum.org>
2020-02-19 15:14:58 +0100

    Fix handling of unused bits in LEB128 decoder
    Co-authored-by: Paweł Bylica <chfast@gmail.com>

--- lib/fizzy/leb128.hpp
+++ lib/fizzy/leb128.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "exceptions.hpp"
-#include "types.hpp"
 #include <cstdint>
+#include <limits>
 
 namespace fizzy
 {
@@ -19,11 +19,14 @@ std::pair<T, const uint8_t*> leb128u_decode(const uint8_t* input, const uint8_t*
         if (input == end)
             throw parser_error("Unexpected EOF");
 
-        // TODO this ignores the bits in the last byte other than the least significant one
-        // So would not reject some invalid encoding with those bits set.
         result |= static_cast<T>((static_cast<T>(*input) & 0x7F) << result_shift);
         if ((*input & 0x80) == 0)
+        {
+            if (*input != (result >> result_shift))
+                throw parser_error("Invalid LEB128 encoding: unused bits set.");
+
             return {result, input + 1};
+        }
     }
 
     throw parser_error("Invalid LEB128 encoding: too many bytes.");
@@ -46,13 +49,31 @@ std::pair<T, const uint8_t*> leb128s_decode(const uint8_t* input, const uint8_t*
         result |= static_cast<T_unsigned>((static_cast<T_unsigned>(*input) & 0x7F) << result_shift);
         if ((*input & 0x80) == 0)
         {
-            // sign extend
-            if ((*input & 0x40) != 0 && (result_shift + 7 < sizeof(T_unsigned) * 8))
+            constexpr auto all_ones = std::numeric_limits<T_unsigned>::max();
+
+            if (result_shift + 7 < sizeof(T_unsigned) * 8)
             {
-                constexpr auto all_ones = std::numeric_limits<T_unsigned>::max();
-                const auto mask = static_cast<T_unsigned>(all_ones << (result_shift + 7));
-                result |= mask;
+                // non-terminal byte of the encoding, extend the sign bit, if needed
+                if ((*input & 0x40) != 0)
+                {
+                    const auto mask = static_cast<T_unsigned>(all_ones << (result_shift + 7));
+                    result |= mask;
+                }
             }
+            else
+            {
+                // terminal byte of the encoding, need to check unused bits
+                const auto unused_bits_mask = ~static_cast<uint8_t>(all_ones >> result_shift);
+                // unused bits must be set if and only if sign bit is set
+                const auto unused_bits_expected =
+                    static_cast<T>(result) < 0 ? (unused_bits_mask & 0x7F) : 0;
+                if ((*input & unused_bits_mask) != unused_bits_expected)
+                {
+                    throw parser_error(
+                        "Invalid LEB128 encoding: unused bits not equal to sign bit.");
+                }
+            }
+
             return {static_cast<T>(result), input + 1};
         }
     }
