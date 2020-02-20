5bc1b810bae467d614eba5ae0946947d790cc248
Alex Beregszaszi <alex@rtfs.hu>
2020-02-20 12:52:33 +0000

    tests: add test cases for UTF8 validation

---
+++ test/unittests/utf8_test.cpp
@@ -0,0 +1,23 @@
+#include "utf8.hpp"
+#include <gtest/gtest.h>
+#include <test/utils/asserts.hpp>
+#include <test/utils/hex.hpp>
+
+using namespace fizzy;
+
+TEST(utf8, validate)
+{
+    std::vector<std::pair<bytes, bool>> testcases = {
+        {"00"_bytes, true},
+        {"7f"_bytes, true},
+        {"80"_bytes, false},
+        {"c2"_bytes, false},
+        {"c280"_bytes, true},
+        {"c2bf"_bytes, true},
+        {"c2e0"_bytes, false},
+    };
+    for (auto const& testcase : testcases)
+    {
+        EXPECT_EQ(utf8_validate(testcase.first), testcase.second);
+    }
+}
